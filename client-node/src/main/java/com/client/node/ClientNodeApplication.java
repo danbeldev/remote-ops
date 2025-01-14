78cc5aa90855b97024d0bcb983a0ccd753a74906
danbel <dan.bel.89@bk.ru>
2025-01-14 04:53:43 +0400

    init

---
+++ client-node/src/main/java/com/client/node/ClientNodeApplication.java
@@ -0,0 +1,13 @@
+package com.client.node;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientNodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientNodeApplication.class, args);
+    }
+
+}
---
+++ client-node/src/test/java/com/client/node/ClientNodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.client.node;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientNodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
